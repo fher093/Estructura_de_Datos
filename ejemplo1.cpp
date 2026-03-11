4944bfaeadaa209629e137cae557af7b9ffa7770
Fernando <fherdavidlleru@gmail.com>
2026-03-11 10:32:40 -0500

    ejemplo 1

---
+++ ejemplo1.cpp
@@ -0,0 +1,19 @@
+#include <iostream> 
+using namespace std; 
+
+int main(){
+    int notas[5] = {8, 9, 7, 10, 6};  
+    double promedio;
+    double suma = 0;
+    
+    for(int i = 0; i < 5; i++){
+        cout << "Nota " << i+1 << ": " << notas[i] << endl; 
+        suma = suma + notas[i];  
+    }  
+    
+    promedio = suma / 5;  
+    
+    cout << "Promedio: " << promedio << endl;
+    
+    return 0;
+}
